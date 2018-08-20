5958baa966ca06d716e91fc10880578d29707224
catherine bradley and Regina Nurieva <regina@>
2018-08-20 11:06:13 -0700

    add inital setup and places constructor with input fields

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function Places(country, city, season, company, sights, notes  ) {
+  this.visitedCountry = country;
+  this.visitedCity = city;
+  this.visitedSeason = season;
+  this.visitedCompany = company;
+  this.visitedSights = sights;
+  this.visitedNotes = notes;
+
+}
