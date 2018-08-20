b571b66396a6e85baefb2d979419354575ef2e12
catherine bradley and Regina Nurieva <regina@>
2018-08-20 14:12:19 -0700

    add more information to display

---
+++ README.md
@@ -0,0 +1,40 @@
+# _Places You've Been_
+
+#### _Week 4 Epicodus Project, 8.20.2018_
+
+#### By _**Catherine Bradley and Regina Nurieva **_
+
+## Description
+
+_This is an interactive webpage designed to demonstrate our skills using Object-Oriented JavaScript Objectives such as constructors and prototypes. The goal of this project is to .....  Additionally we use personal CSS stylings and Bootstrap to give the webpage as travel blog theme._
+
+### Specifications
+* _1) Program takes information on travel, inputted by user, and displays that information on screen in a new format._
+  * _**Example Input: Paris_
+  * _**Example Input: France_
+  * _**Example Output: Paris, France**_
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open the Index.html file in Chrome web browser_
+
+_This webpage will likely work as expected in any web browser but specifically was designed for Chrome._
+
+## Known Bugs
+
+_     _
+
+## Support and contact details
+
+_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+
+## Technologies Used
+
+_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+
+### License
+
+*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+
+Copyright (c) 2016 **_{List of contributors or company name}_**
--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,20 @@ function Places(country, city, season, company, sights, notes) {
 }
 
 Places.prototype.fullTravel = function() {
-  return this.visitedCountry + " " + this.visitedCity;
+  return this.visitedCity + ", " + this.visitedCountry;
+}
+
+
+function Details(season, company, sights, notes) {
+  this.visitedSeason = season;
+  this.visitedCompany = company;
+  this.visitedSights = sights;
+  this.visitedNotes = notes;
+
+}
+
+Details.prototype.travelBlog = function() {
+  return "I visted in " + this.visitedSeason + ". " + "Friends who went with me: " + this.visitedCompany + " I saw several things such as: " + this.visitedSights + " My final thoughts are: " + this.visitedNotes;
 }
 
 
@@ -22,15 +35,25 @@ $(document).ready(function() {
 
     var inputtedVisitedCountry = $("#new-country").val();
     var inputtedVisitedCity = $("#new-city").val();
+    var newPlaces = new Places(inputtedVisitedCountry, inputtedVisitedCity);
+
     var inputtedVisitedSeason = $("#new-season").val();
     var inputtedVisitedCompany = $("#new-company").val();
     var inputtedVisitedSights = $("#new-sights").val();
     var inputtedVisitedNotes = $("#new-notes").val();
-    var newPlaces = new Places(inputtedVisitedCountry, inputtedVisitedCity, inputtedVisitedSeason, inputtedVisitedCompany, inputtedVisitedNotes);
+    var newDetails = new Details(inputtedVisitedSeason, inputtedVisitedCompany, inputtedVisitedSights, inputtedVisitedNotes);
 
     console.log(newPlaces);
+    console.log(newDetails);
+
 
     $("#travelblog h2").text(newPlaces.fullTravel());
 
+    $("#travelblog p").text(newDetails.travelBlog());
+
   });
+  // $("#travelblog h2").toggle(function(){
+  //   $(".travelInfo").show();
+
+  // });
 });
