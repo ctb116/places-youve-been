21f3737841ddf5a50ca85d59803f2cde2bcf8660
catherine bradley and Regina Nurieva <catherine@>
2018-08-20 11:56:17 -0700

    add working htmp display of information inputted

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-function Places(country, city, season, company, sights, notes  ) {
+// business logic
+function Places(country, city, season, company, sights, notes) {
   this.visitedCountry = country;
   this.visitedCity = city;
   this.visitedSeason = season;
@@ -7,3 +8,29 @@ function Places(country, city, season, company, sights, notes  ) {
   this.visitedNotes = notes;
 
 }
+
+Places.prototype.fullTravel = function() {
+  return this.visitedCountry + " " + this.visitedCity;
+}
+
+
+// front end
+$(document).ready(function() {
+
+  $("form#travel").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedVisitedCountry = $("#new-country").val();
+    var inputtedVisitedCity = $("#new-city").val();
+    var inputtedVisitedSeason = $("#new-season").val();
+    var inputtedVisitedCompany = $("#new-company").val();
+    var inputtedVisitedSights = $("#new-sights").val();
+    var inputtedVisitedNotes = $("#new-notes").val();
+    var newPlaces = new Places(inputtedVisitedCountry, inputtedVisitedCity, inputtedVisitedSeason, inputtedVisitedCompany, inputtedVisitedNotes);
+
+    console.log(newPlaces);
+
+    $("#travelblog h2").text(newPlaces.fullTravel());
+
+  });
+});
